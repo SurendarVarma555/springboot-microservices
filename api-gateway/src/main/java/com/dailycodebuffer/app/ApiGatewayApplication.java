ae34d61772992138a52727babc69973cb02ec852
Surendar <batthulasurendar@gmail.com>
2024-07-24 20:54:41 +0530

    api gateway service implementation

--- api-gateway/src/main/java/com/dailycodebuffer/app/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/dailycodebuffer/app/ApiGatewayApplication.java
@@ -2,12 +2,16 @@ package com.dailycodebuffer.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+@EnableDiscoveryClient
+@EnableHystrix
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main (String[] args){
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
 }
